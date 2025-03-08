40db698cd0daa1b1d99bafdd5262e50492589583
rdeloffe <draphael59400@gmail.com>
2025-03-08 11:35:10 +0100

    Set up fini + Lancement site

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Worldddddddddd!");
+    public static void main(String[] args) throws InterruptedException{
+        System.setProperty("webdriver.chrome.driver","C:\\Browser drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://hightest.nc/");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.close();
     }
 }
