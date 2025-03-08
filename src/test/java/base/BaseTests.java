b13429bda1c0030c5ec2e69a3061ec7fc51df3a7
rdeloffe <draphael59400@gmail.com>
2025-03-08 16:51:15 +0100

    Second test select option

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,21 +1,32 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        System.out.println(driver.getTitle());
-//        driver.quit();
+
+        homePage = new HomePage(driver);
+        //Aller sur le site* cliquer sur "Shifting Content"* puis cliquer sur le premier element "Menu Element" et Systemprint le nombre de menu
     }
-    public static void main(String[] args) throws InterruptedException{
-        BaseTests test = new BaseTests();
-        test.setUp();
+
+    @AfterClass
+    public void teadDown(){
+        driver.quit();
     }
 }
