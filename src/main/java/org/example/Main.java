92d49dc2357e979dfd883186977a948c4d5b561c
rdeloffe <draphael59400@gmail.com>
2025-03-08 11:51:56 +0100

    Premier test get title

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException{
-        System.setProperty("webdriver.chrome.driver","C:\\Browser drivers\\chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://hightest.nc/");
-        driver.manage().window().maximize();
-        Thread.sleep(3000);
-        driver.close();
+        System.out.print("HelloWorld");
     }
 }
---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,21 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+//        driver.quit();
+    }
+    public static void main(String[] args) throws InterruptedException{
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
