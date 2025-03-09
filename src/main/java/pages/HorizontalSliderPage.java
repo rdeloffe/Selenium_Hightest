8e6f6067122780576898ae2ad956f22af1484af5
rdeloffe <draphael59400@gmail.com>
2025-03-09 15:02:22 +0100

    Test Keys Presses

---
+++ src/main/java/pages/HorizontalSliderPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HorizontalSliderPage {
+    private WebDriver driver;
+
+    public HorizontalSliderPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    
+}
