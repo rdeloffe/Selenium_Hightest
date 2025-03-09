9b601faa621c3868953a66d3f88305ad9806d623
rdeloffe <draphael59400@gmail.com>
2025-03-09 14:31:45 +0100

    Test Hover

---
+++ src/main/java/pages/KeyPressesPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class KeyPressesPage {
+    private WebDriver driver;
+
+    public KeyPressesPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+
+}
---
+++ src/test/java/hover/HoverTests.java
@@ -0,0 +1,19 @@
+package hover;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class HoverTests extends BaseTests {
+
+    @Test
+    public void testHoverUser1(){
+        var hoverPage = homePage.clickHovers();
+        var caption = hoverPage.hoverOverFigure(1);
+        assertTrue(caption.isCaptionDisplayed(), "Caption not dispalyed");
+        assertEquals(caption.getTitle(),"name: user1","Caption title incorrect");
+        assertEquals(caption.getLinkText(),"View profile" , "Caption link test incorrect");
+        assertTrue(caption.getLink().endsWith("/users/1"), "Link Incorrect");
+    }
+}
